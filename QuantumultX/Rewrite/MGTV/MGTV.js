4202a6ef92f591802018d9efee521cd7997ccb92
wzt7 <110069925+wztt7@users.noreply.github.com>
2024-11-16 03:40:20 +0800

    Update MGTV.js

--- QuantumultX/Rewrite/MGTV/MGTV.js
+++ QuantumultX/Rewrite/MGTV/MGTV.js
@@ -12,8 +12,7 @@
 ^https?:\/\/mobile-stream\.api\.mgtv\.com\/v1\/video\/source\? url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/mgtk.js
 ^https?://mobile.da.mgtv.com\/m\/page url reject
 # pad
-^https?:\/\/pad\.api\.mgtv\.com\/v1\/video\/getSource\? url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/mgtk.js
-
+^https?:\/\/pad\.api\.mgtv\.com\/v1\/video\/source\? url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/mgtk.js
 [mitm] 
 hostname = *.mgtv.com, pad.api.mgtv.com
 *
